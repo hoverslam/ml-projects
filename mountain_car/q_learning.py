733a5f3486bdbb63cdf3e404fecaad613bb23ad7
hoverslam <hofmann.christopher@outlook.com>
2022-06-19 00:52:34 +0200

    Add class for gym environment

--- mountain_car/q_learning.py
+++ mountain_car/q_learning.py
@@ -1,10 +1,10 @@
+import json
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-# Q-learning agent
 class MountainCarDriver():
     
     def __init__(self, alpha: float, gamma: float, epsilon: float, 
@@ -43,53 +43,95 @@ class MountainCarDriver():
         else:
             return np.argmax(self.Q[self.get_state(obs)])
         
-    def save_table(self, file: str):
-        np.save(file, self.Q)
+    def save_table(self, filepath: str, filename: str):
+        np.save("{}/{}".format(filepath, filename), self.Q)
         
-    def load_table(self, file: str):
-        self.Q = np.load(file)
-        
-
-# Settings
-EPISODES = 10000
-SHOW_EVERY = 1000
-
-
-# Initialize environment
-env = gym.make("MountainCar-v0")
-player = MountainCarDriver(alpha=0.1, gamma=0.99, epsilon=0.1, 
-                           num_actions=3, num_inputs=2, bin_size=30)
-   
-                  
-# Training
-history = {"episode": [], "reward" : []}
-for episode in tqdm(range(EPISODES)):
-    obs = env.reset()
-    done = False
-    episode_reward = 0
+    def load_table(self, filepath:str, filename: str):
+        self.Q = np.load("{}/{}.npy".format(filepath, filename))
     
-    while not done:
-        action = player.policy(obs)
-        new_obs, reward, done, _ = env.step(action)
-        new_obs = new_obs
-        episode_reward += reward
-        
-        player.update_table(reward, action, obs, new_obs)        
-        obs = new_obs
-        
-        if ((episode + 1) % SHOW_EVERY == 0):
-            env.render()
+
+class GymMountainCar():
     
-    history["episode"].append(episode+1)
-    history["reward"].append(episode_reward)
+    def __init__(self) -> None:
+        # Initialize environment
+        self.env = gym.make("MountainCar-v0")
+        self.num_actions = 3
+        self.num_inputs = 2
+        
+        
+        # Initialize agent
+        self.agent = MountainCarDriver(alpha=0.1, gamma=0.95, epsilon=0.1, bin_size=30, 
+                                       num_actions=self.num_actions, num_inputs=self.num_inputs)
+        
+        # Stats
+        self.history = {"episode": [], "reward": []}
+
+    def train_agent(self, num_episodes: int, show_every: int) -> None:
+        for episode in tqdm(range(num_episodes)):
+            obs = self.env.reset()
+            done = False
+            episode_reward = 0
+                
+            while not done:
+                action = self.agent.policy(obs)
+                new_obs, reward, done, _ = self.env.step(action)
+                new_obs = new_obs
+                episode_reward += reward
+                
+                self.agent.update_table(reward, action, obs, new_obs)        
+                obs = new_obs
+                
+                if ((episode + 1) % show_every == 0):
+                    self.env.render()
+                    
+            self.history["episode"].append(episode+1)
+            self.history["reward"].append(episode_reward)
+            
+    def play(self, num_episodes, show=True):
+        for episode in range(num_episodes):
+            obs = self.env.reset()
+            done = False
+            episode_reward = 0
+            
+            while not done:
+                action = self.agent.policy(obs)
+                obs, reward, done, _ = self.env.step(action)
+                episode_reward += reward
+                    
+                if show:
+                    self.env.render()
+                    
+            print("Reward: {:.2f} at episode {}".format(episode_reward, episode+1))
+                    
+    def plot_performance(self):
+        rewards = self.history["reward"]
+        mov_avg = [np.average(rewards[:i+1]) for i in range(len(rewards))]
+        mov_avg_100 = mov_avg[:100] + [np.average(rewards[i:i+100]) for i in range(len(rewards)-100)]
+
+        plt.scatter(self.history["episode"], rewards, s=1, alpha=0.5)
+        plt.plot(self.history["episode"], mov_avg_100, color="red", alpha=0.7)
+        plt.xlabel("Episode")
+        plt.ylabel("Reward")
+        plt.show()
+
+    def save_agent(self, filepath, filename, stats=True):
+        self.agent.save_table(filepath, filename)
+        if stats:
+            src = "{}/{}".format(filepath, filename)
+            with open("{}_stats.json".format(src), "w") as f:
+                f.write(json.dumps(self.history))
+                f.close()
+        
+    def load_agent(self, filepath, filename, stats=True):        
+        self.agent.load_table(filepath, filename)
+        if stats:
+            src = "{}/{}".format(filepath, filename)
+            self.history = json.load(open("{}_stats.json".format(src)))
 
 
-# Plot results
-mov_avg = [np.average(history["reward"][:i+1]) for i in range(len(history["reward"]))]
-mov_avg_100 = mov_avg[:100] + [np.average(history["reward"][i:i+100]) for i in range(len(history["reward"])-100)]
-
-plt.scatter(history["episode"], history["reward"], s=1, alpha=0.5)
-plt.plot(history["episode"], mov_avg_100, color="red", alpha=0.7)
-plt.xlabel("Episode")
-plt.ylabel("Reward")
-plt.show()
+if __name__ == "__main__":
+    g = GymMountainCar()
+    g.train_agent(5000, 1000)
+    g.save_agent("mountain_car", "q_temp")
+    g.plot_performance()
+    
